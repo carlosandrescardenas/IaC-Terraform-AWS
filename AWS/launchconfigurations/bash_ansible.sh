635e13461ce42662858c05bd8442355500d4a01f
carlosandrescardenas <andres2158@hotmail.com>
2021-11-10 18:34:13 -0500

    file updated

--- AWS/launchconfigurations/bash_ansible.sh
+++ AWS/launchconfigurations/bash_ansible.sh
@@ -1,8 +1,9 @@
+#!/bin/bash
+
 apt update
 apt upgrade
 add-apt-repository ppa:ansible/ansible
 apt update
 apt install ansible -y
-apt upgrade
 git clone https://github.com/carlosandrescardenas/ansible-configuration.git
 cd /ansible-configuration/
